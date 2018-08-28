096a8763dfe9081ee72fd41f17b0ec50f8ad21db
Ignacio R Espino <irespino@capozios.com>
2018-08-28 20:58:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
+  reversed_array = []
   array.collect do |item|
-    item.reverse
+    reversed_array.push(item.reverse)
   end
-  return array.join(" ")
+  return reversed_array.join(" ")
 end
